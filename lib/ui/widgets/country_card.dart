7ce2b6295b09a4826d75842613756eda604e129b
AlbaneCar <albane.carteron@gmail.com>
2025-01-06 15:02:45 +0100

    add Country Flag

--- lib/ui/widgets/country_card.dart
+++ lib/ui/widgets/country_card.dart
@@ -9,16 +9,62 @@ class CountryCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            country.strArea,
-            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          if (country.strArea != 'Unknown')
+            Expanded(
+              child: Image.network(
+                getFlagUrl(country.strArea),
+                fit: BoxFit.cover,
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              country.strArea,
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
           ),
-        ),
+        ],
       ),
     );
+  }
+
+  String getFlagUrl(String country) {
+    final countryCodes = {
+      'American': 'us',
+      'British': 'gb',
+      'Canadian': 'ca',
+      'Chinese': 'cn',
+      'Croatian': 'hr',
+      'Dutch': 'nl',
+      'Egyptian': 'eg',
+      'Filipino': 'ph',
+      'French': 'fr',
+      'Greek': 'gr',
+      'Indian': 'in',
+      'Irish': 'ie',
+      'Italian': 'it',
+      'Jamaican': 'jm',
+      'Japanese': 'jp',
+      'Kenyan': 'ke',
+      'Malaysian': 'my',
+      'Mexican': 'mx',
+      'Moroccan': 'ma',
+      'Polish': 'pl',
+      'Portuguese': 'pt',
+      'Russian': 'ru',
+      'Spanish': 'es',
+      'Thai': 'th',
+      'Tunisian': 'tn',
+      'Turkish': 'tr',
+      'Ukrainian': 'ua',
+      'Vietnamese': 'vn',
+    };
+
+    final code = countryCodes[country] ?? 'unknown';
+    return 'https://www.themealdb.com/images/icons/flags/big/64/$code.png';
   }
 }
